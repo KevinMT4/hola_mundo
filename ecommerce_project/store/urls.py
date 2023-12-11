70853618ef2c4b0890e7b8a6aab4d1e44a0bbdd2
KevinMT4 <144923295+KevinMT4@users.noreply.github.com>
2023-12-11 00:13:21 -0600

    Add files via upload

--- ecommerce_project/store/urls.py
+++ ecommerce_project/store/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('product_list/', views.product_list, name='product_list'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('view_cart/', views.view_cart, name='view_cart'),
+     path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('add_product/', views.add_product, name='add_product'),
 ]
--- ecommerce_project/store/views.py
+++ ecommerce_project/store/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
 from django.shortcuts import render, redirect
+
+from store.forms import ProductForm
 from .models import Product, Order
 
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'store/product_list.html', {'products': products})
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+
+    return render(request, 'store/add_product.html', {'form': form})
+
+
 
 def add_to_cart(request, product_id):
     product = Product.objects.get(id=product_id)
@@ -15,6 +29,10 @@ def add_to_cart(request, product_id):
     order.products.add(product)
     order.total_price += product.price
     order.save()
+    return redirect('product_list')
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    product.delete()
     return redirect('product_list')
 
 def view_cart(request):
